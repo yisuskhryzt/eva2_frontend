ba64aa88ab6a3d1eb73d91a66f863e66ef9f9829
ElCorbacho <histopat.cor@gmail.com>
2025-05-12 23:36:49 -0400

    componente galeria

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ import './Home.css';
 import QuickLinks from './QuickLinks';
 import Services from './Services';
 import News from './News';
+import Galeria from './Galeria/Galeria';
+
 
 const Home = () => {
   return (
@@ -24,22 +26,8 @@ const Home = () => {
         <QuickLinks />
       </section>
 
-      <section className="galeria py-5">
-        <Container fluid>  {/* Usa 'fluid' para que el contenedor ocupe todo el ancho disponible */}
-          <h2 className="text-center mb-4">Galería de Imágenes</h2>
-          <Carousel fade>
-            <Carousel.Item>
-              <img className="d-block carousel-img" src="/logo_cholchol.png" alt="Imagen 1" />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block carousel-img" src="/fondo.jpg" alt="Imagen 2" />
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block carousel-img" src="/entrada.jpg" alt="Imagen 3" />
-            </Carousel.Item>
-          </Carousel>
-        </Container>
-      </section>
+      {/* Usar el nuevo componente Galeria */}
+      <Galeria/>
 
     </div>
   );
